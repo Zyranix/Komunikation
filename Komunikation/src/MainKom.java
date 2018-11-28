76a18da3ace606144be9717811a2d17db90fab8c
Alexander <Alexander@Alex-Laptop>
2018-11-28 13:56:12 +0100

    adding javafx

--- Komunikation/src/MainKom.java
+++ Komunikation/src/MainKom.java
@@ -1,4 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class MainKom {
-
+public class MainKom extends Application{
+	
+	Stage window;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		window = primaryStage;
+		
+	}
 }
